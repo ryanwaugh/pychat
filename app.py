0ae96ed4e5f861755ecaee74f3e6072b8ffcc670
Ryan Waugh <rmwguitar@gmail.com>
2025-11-11 21:54:02 -0400

    fixed: flask use port 5001 to prevent conflict w/ nginx port 5000, add python deps

--- app.py
+++ app.py
@@ -172,4 +172,4 @@ def handle_message(data):
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5001, debug=True)
